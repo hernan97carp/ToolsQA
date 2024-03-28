8a8d552243d486eaad27154324d232514a88d829
unknown <hernan97carp@hotmail.com>
2024-03-27 23:36:57 -0300

    new pipeline cypress, modify the second test case of Elements page and started to create the test case of  menu Page

--- cypress/support/POM/widgets/menu.Page.js
+++ cypress/support/POM/widgets/menu.Page.js
@@ -1,6 +1,18 @@
 class MenuItems {
-	mainItems = {
-		firstMainItem: () => cy.get('a[href="#"]'.eq(0)),
-		SecondMainItem: () => cy.get('a[href="#"]'.eq(1)),
+	mainItemOne = {
+		firstMainItem: () => cy.get('a[href="#"]'),
+	};
+	mainItemTwo = {
+		SecondMainItem: () => cy.get('a[href="#"]'),
+		secondMainFirstSubItem: () => cy.get('a[href="#"]'),
+		secondMainSecondSubItem: () => cy.get('a[href="#"]'.eq(3)),
+		secondMainSubSubList: () => cy.get('a[href="#"]'.eq(4)),
+		secondMainSubSubItemOne: () => cy.get('a[href="#"]'.eq(5)),
+		secondMainSubSubItemTwo: () => cy.get('a[href="#"]'.eq(6)),
+	};
+	mainItemThree = {
+		thirdMainItem: () => cy.get('a[href="#"]'.eq(7)),
 	};
 }
+
+export const menuItems = new MenuItems();
