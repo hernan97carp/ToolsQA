00a1fc9cf1eb737274d5bae1ce82dbb22d4d6fff
hernan <hernan97carp@hotmail.com>
2024-04-10 11:07:44 -0300

    add a new test in the page Sortable Of the list elements

--- cypress/e2e/Tests/interactions/droppable.cy.js
+++ cypress/e2e/Tests/interactions/droppable.cy.js
@@ -1,11 +1,6 @@
-const { it } = require('mocha');
-
 describe('Cypress Challenge', () => {
 	beforeEach(() => {
 		cy.visit('/droppable');
 	});
-
-	it('This is your test case one title', () => {});
-	it('This is your test case one title', () => {});
-	it('This is your test case one title', () => {});
+	it('', () => {});
 });
--- cypress/e2e/Tests/interactions/sortable.cy.js
+++ cypress/e2e/Tests/interactions/sortable.cy.js
@@ -1,5 +1,4 @@
 import { Sortable } from '../../../support/POM/intereactions/sortable';
-import faker from 'faker';
 describe('Cypress Challenge Sortable', () => {
 	const sortable = new Sortable();
 	const list = sortable.list;
@@ -7,28 +6,28 @@ describe('Cypress Challenge Sortable', () => {
 		cy.visit('/sortable');
 	});
 
-	it('TC1:', () => {
-		const elements = [];
-		function randomTwoNumbers() {
-			let numero1 = faker.datatype.number({ min: 0, max: 5 });
-			let numero2 = faker.datatype.number({ min: 0, max: 5 });
-			while (numero1 === numero2) {
-				numero2 = faker.datatype.number({ min: 0, max: 5 });
-			}
-			return [numero1, numero2];
-		}
-		const [aleatoryNumberOne, aleatoryNumberTwo] = randomTwoNumbers();
-		cy.get(list.elements)
-			.each(($el, index) => {
-				if (index <= 6) {
-					elements.push($el);
-				}
-			})
-			.then(() => {
-				cy.wrap(elements).then(elements => {
-					cy.get(elements[aleatoryNumberOne]).trigger('mousedown');
-					cy.get(elements[aleatoryNumberTwo]).trigger('mousemove').trigger('mouseup');
+	it('TC1: Verify Element Relocation', () => {
+		cy.twoNumbersRandom().then(random => {
+			const numberOne = random[0]; // Accede al primer número
+			const numberTwo = random[1]; // Accede al segundo número
+			const elements = [];
+			let nameOfElement = [];
+
+			cy.get(list.elements)
+				.each(($el, index) => {
+					if (index <= 6) {
+						elements.push($el);
+						nameOfElement.push($el.text());
+					}
+				})
+				.then(() => {
+					cy.wrap(elements).then(elements => {
+						cy.step(`The ${nameOfElement[numberOne]} element was moved`);
+						cy.get(elements[numberOne]).trigger('mousedown');
+						cy.step(`To the position of the element ${nameOfElement[numberTwo]}`);
+						cy.get(elements[numberTwo]).trigger('mousemove').trigger('mouseup');
+					});
 				});
-			});
+		});
 	});
 });
